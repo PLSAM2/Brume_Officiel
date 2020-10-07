3a295002327716be5fdf3ad3f8753dcc177ffb12
Sam <sam.arachtingi@gmail.com>
2020-10-07 15:51:40 +0200

    no message

--- Assets/_Scripts/Tags.cs
+++ Assets/_Scripts/Tags.cs
@@ -10,4 +10,9 @@ public class Tags
     public static readonly ushort JoinRoom = 30;
     public static readonly ushort PlayerJoinedRoom = 40;
     public static readonly ushort QuitRoom = 50;
+    public static readonly ushort SpawnObjPlayer = 60;
+    public static readonly ushort MovePlayerTag = 70;
+    public static readonly ushort SupprObjPlayer = 80;
+
+
 }
