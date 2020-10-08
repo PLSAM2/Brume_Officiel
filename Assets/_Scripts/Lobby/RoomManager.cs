26ddb3a844bb246541548c9dd3e263a43b1fdee6
ETU\v.betrancourt <valentin.betrancourt@hotmail.fr>
2020-10-08 10:12:20 +0200

    RoomManager

---
+++ Assets/_Scripts/Lobby/RoomManager.cs
@@ -0,0 +1,99 @@
+﻿using DarkRift;
+using DarkRift.Client;
+using DarkRift.Client.Unity;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RoomManager : MonoBehaviour
+{
+    private static RoomManager _instance;
+    public static RoomManager Instance { get { return _instance; } }
+
+    [SerializeField] UnityClient client;
+
+    public RoomData actualRoom;
+    public string gameScene;
+    public string menuScene;
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+
+        client.MessageReceived += MessageReceived;
+    }
+
+    private void MessageReceived(object sender, MessageReceivedEventArgs e)
+    {
+        using (Message message = e.GetMessage() as Message)
+        {
+            if (message.Tag == Tags.StartGame)
+            {
+                StartGameInServer(sender, e);
+            }
+            if (message.Tag == Tags.QuitGame)
+            {
+                QuitGameInServer(sender, e);
+            }
+        }
+    }
+
+    public void StartGame()
+    {
+        using (DarkRiftWriter writer = DarkRiftWriter.Create())
+        {
+            writer.Write(actualRoom.ID);
+
+            using (Message message = Message.Create(Tags.StartGame, writer))
+                client.SendMessage(message, SendMode.Reliable);
+        }
+    }
+
+    private void StartGameInServer(object sender, MessageReceivedEventArgs e)
+    {
+        SceneManager.LoadScene(gameScene, LoadSceneMode.Single);
+    }
+
+    public void QuitGame()
+    {
+        using (DarkRiftWriter writer = DarkRiftWriter.Create())
+        {
+            writer.Write(actualRoom.ID);
+            print("Quit");
+            using (Message message = Message.Create(Tags.QuitGame, writer))
+                client.SendMessage(message, SendMode.Reliable);
+        }
+    }
+
+    private void QuitGameInServer(object sender, MessageReceivedEventArgs e)
+    {
+        SceneManager.LoadScene(menuScene, LoadSceneMode.Single);
+    }
+
+
+    public List<PlayerData> GetAllPlayerInActualRoom()
+    {
+        List<PlayerData> _playerList = new List<PlayerData>();
+
+        foreach (KeyValuePair<ushort, PlayerData> player in actualRoom.playerList)
+        {
+            _playerList.Add(player.Value);
+        }
+
+        return _playerList;
+    }
+
+
+
+}
