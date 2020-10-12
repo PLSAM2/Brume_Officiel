a5861ce9689d8485af91c135098cd04e7d6c1a6b
Sam <sam.arachtingi@gmail.com>
2020-10-12 09:35:24 +0200

    fix tag

--- Assets/_Scripts/Tags.cs
+++ Assets/_Scripts/Tags.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Tags 
 {
+    // Lobby >>       
     public static readonly ushort PlayerConnected = 0;
     public static readonly ushort Ping = 2;
     public static readonly ushort SendAllRooms = 5;
@@ -14,13 +15,18 @@ public class Tags
     public static readonly ushort PlayerJoinedRoom = 40;
     public static readonly ushort PlayerQuitRoom = 45;
     public static readonly ushort QuitRoom = 50;
-    public static readonly ushort SpawnObjPlayer = 60;
-    public static readonly ushort MovePlayerTag = 70;
-    public static readonly ushort SupprObjPlayer = 80;
     public static readonly ushort StartGame = 90;
     public static readonly ushort QuitGame = 100;
     public static readonly ushort ChangeName = 110;
     public static readonly ushort ChangeTeam = 120;
     public static readonly ushort SetReady = 130;
     public static readonly ushort SetCharacter = 140;
+    // <<
+
+    // InGame >>   
+    public static readonly ushort SpawnObjPlayer = 1000;
+    public static readonly ushort MovePlayerTag = 1010;
+    public static readonly ushort SupprObjPlayer = 1020;
+    public static readonly ushort SendAnim = 1030;
+    // <<
 }
