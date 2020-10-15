a668b86e4e326e7ee675e6362b8dd52e8e1879d8
Sam <sam.arachtingi@gmail.com>
2020-10-15 18:18:03 +0200

    no message

---
+++ Assets/LitParticles/LightFlickerFast.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+
+var minFlickerSpeed : float = 0.1;
+var maxFlickerSpeed : float = 2.0;
+
+function Update () {//
+	flicker();
+}
+
+ function flicker() {
+ 	//yield return new WaitForSeconds(Random.Range(minF
+ 	yield WaitForSeconds (Random.Range(minFlickerSpeed, maxFlickerSpeed ));
+     GetComponent.<Light>().intensity = (Random.Range(minFlickerSpeed, maxFlickerSpeed ));
+     //yield WaitForSeconds (Random.Range(minFlickerSpeed, maxFlickerSpeed ));
+     //light.intensity = (Random.Range(minFlickerSpeed, maxFlickerSpeed ));
+ }
+ 
+ 
+    
