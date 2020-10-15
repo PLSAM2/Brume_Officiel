fc290d531ef0748c04f3777e34e9da7f637fcc9a
ETU\v.betrancourt <valentin.betrancourt@hotmail.fr>
2020-10-15 13:39:49 +0200

    no message

--- Assets/_Scripts/Tags.cs
+++ Assets/_Scripts/Tags.cs
@@ -29,8 +29,10 @@ public class Tags
     public static readonly ushort StopGame = 310;
     public static readonly ushort InstantiateObject = 500;
     public static readonly ushort SynchroniseObject = 510;
+    public static readonly ushort DestroyObject = 515;
     public static readonly ushort Damages = 520;
     public static readonly ushort AddPoints = 550;
+    public static readonly ushort SyncTrigger = 750;
     public static readonly ushort KillCharacter = 560;
     public static readonly ushort SpawnObjPlayer = 1000;
     public static readonly ushort MovePlayerTag = 1010;
