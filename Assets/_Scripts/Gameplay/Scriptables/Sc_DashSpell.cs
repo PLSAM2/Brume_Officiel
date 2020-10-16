951838481233a6ca0ddd907b59653b253e3a6b43
Pierre Soriot <pierre.sorito@gmail.com>
2020-10-16 15:33:17 +0200

    update icon

--- Assets/_Scripts/Gameplay/Scriptables/Sc_DashSpell.cs
+++ Assets/_Scripts/Gameplay/Scriptables/Sc_DashSpell.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "NewSpell", menuName = "CreateCuston/NewSpell/DashSpell")]
 public class Sc_DashSpell : Sc_Spell
 {
+    [Header("Dash Parameters")]
     public float timeToReachMaxRange;
     public float damagesRadius;
 }
