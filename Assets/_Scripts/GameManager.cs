6697e77f7c7b4908d5dd3c315e98c8e4783fcefc
ETU\v.betrancourt <valentin.betrancourt@hotmail.fr>
2020-10-29 17:47:41 +0100

    Le push du confinement

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -25,7 +25,6 @@ public class GameManager : SerializedMonoBehaviour
     public LocalPlayer currentLocalPlayer {get => _currentLocalPlayer; set { _currentLocalPlayer = value; PlayerSpawned.Invoke(_currentLocalPlayer.myPlayerModule); } }
 
     [SerializeField] UnityClient client;
-    [SerializeField] GameObject prefabPlayer;
 
     [Header("Timer")]
     [SerializeField] private float timePerRound = 180; // seconds
@@ -40,12 +39,9 @@ public class GameManager : SerializedMonoBehaviour
     public List<Transform> visiblePlayer = new List<Transform>();
     public static Action<PlayerModule> PlayerSpawned;
 
+
+
     private bool stopInit = false;
-    private bool isWaitingForRespawn = false;
-    private Dictionary<Team, ushort> scores = new Dictionary<Team, ushort>();
-   
-    public static Action AllCharacterSpawned;
-    int numberOfPlayerToSpawn;
     public bool gameStarted = false;
 
     private void Awake()
@@ -70,13 +66,7 @@ public class GameManager : SerializedMonoBehaviour
 
     private void Start()
     {
-        numberOfPlayerToSpawn = RoomManager.Instance.actualRoom.playerList.Count;
-        SendSpawnChamp();
 
-        // A refaire >>
-        scores.Add(Team.blue, 0);
-        scores.Add(Team.red, 0);
-        // <<
         remainingTime = timePerRound;
         int secondRemaining = (int)remainingTime % 60;
         int minuteRemaining = (int)Math.Floor(remainingTime / 60);
@@ -99,19 +89,7 @@ public class GameManager : SerializedMonoBehaviour
     {
         using (Message message = _e.GetMessage() as Message)
         {
-            if (message.Tag == Tags.SpawnObjPlayer)
-            {
-                SpawnPlayerObj(_sender, _e);
-            }
 
-            if (message.Tag == Tags.MovePlayerTag)
-            {
-                SendPlayerMove(_sender, _e);
-            }
-            if (message.Tag == Tags.SupprObjPlayer)
-            {
-                SupprPlayerInServer(_sender, _e);
-            }
             if (message.Tag == Tags.StartTimer)
             {
                 StartTimerInServer();
@@ -120,60 +98,10 @@ public class GameManager : SerializedMonoBehaviour
             {
                 StopGameInServer();
             }
-            if (message.Tag == Tags.AddPoints)
-            {
-                AddPointsInServer(_sender, _e);
-            }
-            if (message.Tag == Tags.KillCharacter)
-            {
-                KillCharacterInServer(_sender, _e);
-            }
-            if (message.Tag == Tags.Damages)
-            {
-                TakeDamagesInServer(_sender, _e);
-            }
-        }
-    }
-
-    public void AddPoints(Team targetTeam, ushort value)
-    {
-        using (DarkRiftWriter _writer = DarkRiftWriter.Create())
-        {
-            _writer.Write((ushort)targetTeam);
-            _writer.Write(value);
-
-            using (Message _message = Message.Create(Tags.AddPoints, _writer))
-            {
-                client.SendMessage(_message, SendMode.Reliable);
-            }
-        }
-    }
-    private void AddPointsInServer(object sender, MessageReceivedEventArgs e)
-    {
-        using (Message message = e.GetMessage())
-        {
-            using (DarkRiftReader reader = message.GetReader())
-            {
-                Team _team = (Team)reader.ReadUInt16();
-                ushort _score = reader.ReadUInt16();
-
-                scores[_team] += _score;
-
-                if (_team == RoomManager.Instance.GetLocalPlayer().playerTeam)
-                {
-                    UiManager.Instance.allyScore.text = scores[_team].ToString();
-
-                    UiManager.Instance.DisplayGeneralPoints(_team, _score);
-
-                } else
-                {
-                    UiManager.Instance.ennemyScore.text = scores[_team].ToString();
-                }
-            }
-
 
         }
     }
+
 
     public void ResetCam()
     {
@@ -216,7 +144,6 @@ public class GameManager : SerializedMonoBehaviour
         }
     }
 
-
     private void StopGameInServer()
     {
         RoomManager.Instance.AlreadyInit = true;
@@ -242,204 +169,5 @@ public class GameManager : SerializedMonoBehaviour
         timeStart = true;
     }
 
-    void SendSpawnChamp()
-    {
-        using (DarkRiftWriter _writer = DarkRiftWriter.Create())
-        {
-            _writer.Write(RoomManager.Instance.actualRoom.ID);
-            _writer.Write(client.ID);
-
-            using (Message _message = Message.Create(Tags.SpawnObjPlayer, _writer))
-            {
-                client.SendMessage(_message, SendMode.Reliable);
-            }
-        }
-    }
-
-    public void KillCharacter()
-    {
-        using (DarkRiftWriter _writer = DarkRiftWriter.Create())
-        {
-            using (Message _message = Message.Create(Tags.KillCharacter, _writer))
-            {
-                client.SendMessage(_message, SendMode.Reliable);
-            }
-        }
-    }
-
-    private void KillCharacterInServer(object sender, MessageReceivedEventArgs e)
-    {
-        using (Message message = e.GetMessage())
-        {
-            using (DarkRiftReader reader = message.GetReader())
-            {
-                ushort id = reader.ReadUInt16();
-
-                SupprPlayer(id);
-
-                if (RoomManager.Instance.GetLocalPlayer().ID == id)
-                {
-                    if (!isWaitingForRespawn)
-                    {
-                        StartCoroutine(WaitForRespawn());
-                    }
-                }
-            }
-        }
-    }
-
-
-    private void TakeDamagesInServer(object sender, MessageReceivedEventArgs e)
-    {
-        using (Message message = e.GetMessage())
-        {
-            using (DarkRiftReader reader = message.GetReader())
-            {
-                ushort _id = reader.ReadUInt16();
-                ushort _damages = reader.ReadUInt16();
-                print(_damages);
-                if (!networkPlayers.ContainsKey(_id))
-                {
-                    return;
-                }
-
-                LocalPlayer target = networkPlayers[_id];
-                print(target.myPlayerId);
-                target.liveHealth -= _damages;
-
-            }
-        }
-
-    }
-
-
-    void SupprPlayerInServer(object _sender, MessageReceivedEventArgs _e)
-    {
-        using (Message message = _e.GetMessage())
-        {
-            using (DarkRiftReader reader = message.GetReader())
-            {
-                ushort id = reader.ReadUInt16();
-                SupprPlayer(id);
-            }
-        }
-    }
-
-    void SupprPlayer(ushort ID)
-    {
-        Destroy(networkPlayers[ID].gameObject);
-        networkPlayers.Remove(ID);
-    }
-
-    void SpawnPlayerObj(object _sender, MessageReceivedEventArgs _e)
-    {
-        using (Message message = _e.GetMessage())
-        {
-            using (DarkRiftReader reader = message.GetReader())
-            {
-                if (message.Tag == Tags.SpawnObjPlayer)
-                {
-                    ushort id = reader.ReadUInt16();
-
-                    if (networkPlayers.ContainsKey(id)) { return; }
-
-                    Vector3 spawnPos = Vector3.zero;
-
-                    foreach (SpawnPoint spawn in spawns[RoomManager.Instance.actualRoom.playerList[id].playerTeam])
-                    {
-                        if (spawn.CanSpawn())
-                        {
-                            spawnPos = spawn.transform.position;
-                        }
-                    }
-
-                    GameObject obj = Instantiate(prefabPlayer, spawnPos, Quaternion.identity) as GameObject;
-                    LocalPlayer myLocalPlayer = obj.GetComponent<LocalPlayer>();
-                    myLocalPlayer.myPlayerId = id;
-                    myLocalPlayer.isOwner = client.ID == id;
-                    myLocalPlayer.Init(client);
-
-
-                    if (myLocalPlayer.isOwner)
-                    {
-                        currentLocalPlayer = myLocalPlayer;
-                    }
-
-                    networkPlayers.Add(id, myLocalPlayer);
-
-                    //CALLBACK TOUS LES JOUEURS SONT APPARUS
-                    numberOfPlayerToSpawn -= 1;
-                    if (numberOfPlayerToSpawn == 0)
-                    {
-                        AllCharacterSpawned.Invoke();
-                        gameStarted = true;
-                    }
-                }
-            }
-        }
-    }
-
-    void SendPlayerMove(object sender, MessageReceivedEventArgs e)
-    {
-        using (Message message = e.GetMessage())
-        {
-            using (DarkRiftReader reader = message.GetReader())
-            {
-                if (message.Tag == Tags.MovePlayerTag)
-                {
-                    ushort id = reader.ReadUInt16();
-
-                    if (!networkPlayers.ContainsKey(id))
-                    {
-                        return;
-                    }
-
-                    networkPlayers[id].SetMovePosition(
-
-                        new Vector3( //Position
-                        reader.ReadSingle(),
-                        networkPlayers[id].transform.position.y,
-                        reader.ReadSingle()),
-
-                        new Vector3( //Rotation
-                        0,
-                        reader.ReadSingle(),
-                        0)
-                   );
-                }
-            }
-        }
-    }
-
-    //void SendPlayerAnim(object sender, MessageReceivedEventArgs e)
-    //{
-    //    using (Message message = e.GetMessage())
-    //    {
-    //        using (DarkRiftReader reader = message.GetReader())
-    //        {
-    //            if (message.Tag == Tags.SendAnim)
-    //            {
-    //                ushort id = reader.ReadUInt16();
-
-    //                if (networkPlayers.ContainsKey(id))
-    //                {
-    //                    networkPlayers[id].SetAnim(
-    //                        reader.ReadSingle(),
-    //                        reader.ReadSingle()
-    //                        );
-    //                }
-
-    //            }
-    //        }
-    //    }
-    //}
-
-
-    IEnumerator WaitForRespawn()
-    {
-        isWaitingForRespawn = true;
-        yield return new WaitForSeconds(currentLocalPlayer.respawnTime);
-        SendSpawnChamp();
-        isWaitingForRespawn = false;
-    }
+    
 }
