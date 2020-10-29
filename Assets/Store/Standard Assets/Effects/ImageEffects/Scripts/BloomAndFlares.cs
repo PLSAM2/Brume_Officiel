0b78ade81df1173d8d2e2aa0bf61ebd25386cc78
Sam <sam.arachtingi@gmail.com>
2020-10-29 14:31:27 +0100

    no message

--- Assets/Store/Standard Assets/Effects/ImageEffects/Scripts/BloomAndFlares.cs
+++ Assets/Store/Standard Assets/Effects/ImageEffects/Scripts/BloomAndFlares.cs
@@ -109,7 +109,7 @@ namespace UnityStandardAssets.ImageEffects
 
             doHdr = false;
             if (hdr == HDRBloomMode.Auto)
-                doHdr = source.format == RenderTextureFormat.ARGBHalf;
+                doHdr = source.format == RenderTextureFormat.ARGBHalf && GetComponent<Camera>().hdr;
             else
             {
                 doHdr = hdr == HDRBloomMode.On;
