068a357b803e8bec872af99214ebd9010a521332
Pierre Soriot <pierre.sorito@gmail.com>
2020-11-10 14:26:34 +0100

    input maj debut cac Attack

---
+++ Assets/_Scripts/Gameplay/Modules/Spells/CacAttack.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CacAttack : SpellModule
+{
+	public override void SetupComponent ( En_SpellInput _actionLinked )
+	{
+		base.SetupComponent(_actionLinked);
+	}
+}
