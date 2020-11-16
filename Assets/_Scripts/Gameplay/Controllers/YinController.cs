f49a77bf461b43044289a6171d0320e43f053cfc
ETU\v.betrancourt <valentin.betrancourt@hotmail.fr>
2020-11-16 18:21:50 +0100

    Fix

---
+++ Assets/_Scripts/Gameplay/Controllers/YinController.cs
@@ -0,0 +1,10 @@
+﻿using DarkRift;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YinController : PlayerModule
+{
+   // [Header("Ying Properties")]
+
+}
