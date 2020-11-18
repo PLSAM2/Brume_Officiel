d1769314a7ddc4065effafafc7a17d29e2bd1514
Pierre Soriot <pierre.sorito@gmail.com>
2020-11-18 18:26:03 +0100

    fix les echos parce que val il jambonne tout

--- Assets/_Scripts/Gameplay/Modules/Essential/PlayerModule.cs
+++ Assets/_Scripts/Gameplay/Modules/Essential/PlayerModule.cs
@@ -363,20 +363,13 @@ public class PlayerModule : MonoBehaviour
         lastRecordedPos = transform.position;
         PlayerModule _localPlayer = GameManager.Instance.currentLocalPlayer.myPlayerModule;
 
-        if (!_localPlayer.isInBrume)
+        if (!_localPlayer.isInBrume || (state &En_CharacterState.Crouched)!=0)
             return false;
 
-        if (Vector3.Distance(transform.position, _localPlayer.transform.position) > _localPlayer.characterParameters.detectionRange)
+        if (Vector3.Distance(transform.position, _localPlayer.transform.position) < _localPlayer.characterParameters.detectionRange)
             return false;
 
-        if (_localPlayer.isInBrume == isInBrume)
-            return false;
 
-        if (Vector3.Distance(_localPlayer.transform.position, transform.position) > _localPlayer.characterParameters.detectionRange)
-            return false;
-
-        if (Vector3.Distance(_localPlayer.transform.position, transform.position) < _localPlayer.characterParameters.visionRange)
-            return false;
 
         return true;
     }
