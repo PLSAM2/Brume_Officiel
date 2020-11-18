e8ea18f4db4a7ad4d9d77bb8608c733e6ac8d970
Pierre Soriot <pierre.sorito@gmail.com>
2020-11-18 15:13:38 +0100

    fix sonar

--- Assets/_Scripts/Gameplay/Modules/Essential/PlayerModule.cs
+++ Assets/_Scripts/Gameplay/Modules/Essential/PlayerModule.cs
@@ -25,7 +25,7 @@ public class PlayerModule : MonoBehaviour
 	public En_CharacterState state
 	{
 		get => _state | LiveEffectCharacterState();
-		set { _state = value;}
+		set { _state = value; }
 	}
 	En_CharacterState LiveEffectCharacterState ()
 	{
@@ -57,7 +57,7 @@ public class PlayerModule : MonoBehaviour
 	Vector3 lastRecordedPos;
 
 	[Header("DamagesPart")]
-	bool _isCrouched =false;
+	bool _isCrouched = false;
 	bool isCrouched
 
 	{ get => _isCrouched; set { _isCrouched = value; if (_isCrouched) { state |= En_CharacterState.Crouched; } else { state = (state & ~En_CharacterState.Crouched); } } }
@@ -337,14 +337,17 @@ public class PlayerModule : MonoBehaviour
 	#region
 	void CheckForBrumeRevelation ()
 	{
+
 		if (GameManager.Instance.currentLocalPlayer == null)
 		{
 			return;
 		}
 		if (ShouldBePinged())
 		{
-			GameObject _fx = Instantiate(sonar, transform.position + Vector3.up, Quaternion.Euler(90, 0, 0));
+			print("I Ping");
+			Instantiate(sonar, transform.position + Vector3.up, Quaternion.Euler(90, 0, 0));
 		}
+		lastRecordedPos = transform.position;
 	}
 
 	bool ShouldBePinged ()
@@ -352,22 +355,12 @@ public class PlayerModule : MonoBehaviour
 		if (lastRecordedPos == transform.position)
 			return false;
 
-		lastRecordedPos = transform.position;
 		PlayerModule _localPlayer = GameManager.Instance.currentLocalPlayer.myPlayerModule;
 
-		if (!_localPlayer.isInBrume)
+		if (!_localPlayer.isInBrume || (state & En_CharacterState.Crouched) != 0)
 			return false;
 
-		if (Vector3.Distance(transform.position, _localPlayer.transform.position) > _localPlayer.characterParameters.detectionRange)
-			return false;
-
-		if (_localPlayer.isInBrume == isInBrume)
-			return false;
-
-		if (Vector3.Distance(_localPlayer.transform.position, transform.position) > _localPlayer.characterParameters.detectionRange)
-			return false;
-
-		if (Vector3.Distance(_localPlayer.transform.position, transform.position) < _localPlayer.characterParameters.visionRange)
+		if (Vector3.Distance(_localPlayer.transform.position, transform.position) >= _localPlayer.characterParameters.detectionRange)
 			return false;
 
 		return true;
@@ -454,9 +447,9 @@ public class PlayerModule : MonoBehaviour
 	{
 		List<EffectLifeTimed> _tempList = new List<EffectLifeTimed>();
 
-		for(int i=0; i < allEffectLive.Count; i++)
+		for (int i = 0; i < allEffectLive.Count; i++)
 		{
-			if(!allEffectLive[i].effect.isConstant)
+			if (!allEffectLive[i].effect.isConstant)
 				allEffectLive[i].liveLifeTime -= Time.fixedDeltaTime;
 
 			if (allEffectLive[i].liveLifeTime <= 0)
@@ -573,7 +566,7 @@ public class EffectLifeTimedTick
 	public float tickRate = 0.2f;
 	[HideInInspector] public float lastTick = 0;
 
-	public void Stop()
+	public void Stop ()
 	{
 		liveLifeTime = 0;
 	}
