b910ce4af5054eb31cdc8e946ec68a16bf6d6205
Sam <sam.arachtingi@gmail.com>
2020-11-19 11:53:27 +0100

    debut killfeed

--- Assets/_Scripts/UI/KillFeed/iconPersoKF.cs
+++ Assets/_Scripts/UI/KillFeed/iconPersoKF.cs
@@ -2,14 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+using static GameData;
 
 public class iconPersoKF : MonoBehaviour
 {
     [SerializeField] Image myIconImg;
-    [SerializeField] TextMeshGUI myUsernameText;
+    [SerializeField] TextMeshProUGUI myUsernameText;
 
-    public void Init(Sprite mySprite, string username)
+    [SerializeField] Sprite iconShili;
+    [SerializeField] Sprite iconYin;
+    [SerializeField] Sprite iconYang;
+
+    public void Init(PlayerData p)
     {
+        switch (p.playerCharacter)
+        {
+            case Character.Shili:
+                myIconImg.sprite = iconShili;
+                break;
 
+            case Character.Yang:
+                myIconImg.sprite = iconYang;
+                break;
+
+            case Character.Yin:
+                myIconImg.sprite = iconYin;
+                break;
+        }
+
+        myUsernameText.text = p.Name;
     }
 }
