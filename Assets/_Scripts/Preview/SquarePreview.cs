310ea2be5ba726770402f1ac4a5c845171920395
Sam <sam.arachtingi@gmail.com>
2020-11-20 17:01:44 +0100

    t pos

---
+++ Assets/_Scripts/Preview/SquarePreview.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SquarePreview : MonoBehaviour
+{
+    [SerializeField] GameObject myObjCenter;
+    [SerializeField] Image myImgCenter;
+
+    [SerializeField] GameObject myObjBorder;
+    [SerializeField] Image myImgBorder;
+
+    public void Init(float _newLenght, float _newWidth, float _newRotation, squareCenter _newCenter, Vector3 _newPos)
+    {
+        Image currentSquare = myImgBorder;
+        GameObject currentObj = myObjBorder;
+
+        if (_newCenter == squareCenter.center) {
+            currentSquare = myImgCenter;
+            currentObj = myObjCenter;
+
+            myObjBorder.SetActive(false);
+            myObjCenter.SetActive(true);
+        }
+        else{
+            myObjBorder.SetActive(true);
+            myObjCenter.SetActive(false);
+        }
+
+        currentObj.transform.localScale = new Vector3(_newWidth, _newLenght, 1);
+        currentObj.transform.localEulerAngles = new Vector3(0, 0, _newRotation);
+
+        currentObj.transform.localPosition = _newPos;
+    }
+
+    public void SetColor(Color _newColor)
+    {
+        myImgCenter.color = _newColor;
+        myImgBorder.color = _newColor;
+    }
+
+    public enum squareCenter
+    {
+        center,
+        border
+    }
+}
---
+++ Assets/_Scripts/Sam/EnemySam.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemySam : MonoBehaviour
+{
+    int life = 50;
+    int maxLife = 50;
+
+    float currentDamageBar = 1;
+
+    [SerializeField] Image lifeBar;
+    [SerializeField] Image damageBar;
+
+    [SerializeField] MeshRenderer myMesh;
+
+    [SerializeField] Material blue;
+    [SerializeField] Material red;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lifeBar.fillAmount = (float)    life / (float)  maxLife;
+        damageBar.fillAmount = (float)life / (float)maxLife;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lifeBar.fillAmount = (float)life / (float)maxLife;
+        damageBar.fillAmount = Mathf.Lerp(damageBar.fillAmount, lifeBar.fillAmount, 3 * Time.deltaTime);
+
+        if(life <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void GetDamage(int damage)
+    {
+        life -= damage;
+        StartCoroutine(AnimDamage());
+    }
+
+    IEnumerator AnimDamage()
+    {
+        myMesh.material = red;
+        yield return new WaitForSeconds(0.4f);
+        myMesh.material = blue;
+    }
+}
