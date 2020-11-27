f1f16de62481fb78723a467a2484ab41c88a45a5
Pierre Soriot <pierre.sorito@gmail.com>
2020-11-27 17:54:18 +0100

    dive wip

---
+++ Assets/_Scripts/Gameplay/Modules/Spells/DiveModule.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiveModule : SpellModule
+{
+	Sc_EffectAtEnd localTrad;
+
+	private void Awake ()
+	{
+		localTrad = spell as Sc_EffectAtEnd;
+	}
+
+	public override void Interrupt ()
+	{
+		base.Interrupt();
+		NetworkObjectsManager.Instance.NetworkInstantiate(NetworkObjectsManager.Instance.GetPoolID(localTrad.objectToSpawnAtThenEnd), transform.position, Vector3.zero);
+	}
+}
---
+++ Assets/_Scripts/Gameplay/Scriptables/Spells/Sc_EffectAtEnd.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+public class Sc_EffectAtEnd : Sc_Spell
+{
+	[TabGroup("EffectAtTheEnd")] public GameObject objectToSpawnAtThenEnd;
+}
