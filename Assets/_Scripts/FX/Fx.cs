7a6aba99831465d5de47dceb35b973c0301eef27
ETU\s.arachtingi <samarachtingi@gmail.com>
2020-12-01 15:40:58 +0100

    fix degat + fix fire

---
+++ Assets/_Scripts/FX/Fx.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fx : MonoBehaviour
+{
+    public bool isVisible = false;
+    public List<GameObject> objToHide = new List<GameObject>();
+
+    private void OnEnable()
+    {
+        GameManager.Instance.allFx.Add(this);
+    }
+
+    private void OnDisable()
+    {
+        GameManager.Instance.allFx.Remove(this);
+        GameManager.Instance.allFx.RemoveAll(x => x == null);
+    }
+}
