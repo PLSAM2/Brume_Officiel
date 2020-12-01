f29915c165a0ce77a68674cbe46b876b643e51b0
Pierre Soriot <pierre.sorito@gmail.com>
2020-12-01 16:14:01 +0100

    auto shili fixed

---
+++ Assets/_Scripts/Gameplay/Scriptables/Spells/Sc_RayAttack.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+[InlineEditor]
+[CreateAssetMenu(fileName = "newRayAttack", menuName = "CreateCuston/newRayAttack")]
+public class Sc_RayAttack : Sc_Spell
+{
+    [TabGroup("DamagePart")] public DamagesInfos damagesToDeal;
+}
