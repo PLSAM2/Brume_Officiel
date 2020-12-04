4183d55783e51a76d0d995e9a6703167d3db8d9d
ETU\s.arachtingi <samarachtingi@gmail.com>
2020-12-04 18:00:27 +0100

    no message

---
+++ Assets/_Scripts/ChampSelect/DescriptionSpell.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DescriptionSpell : MonoBehaviour
+{
+    [SerializeField] Animator myAnimator;
+
+    public void OpenDescription(string _newName, string _newDescription)
+    {
+        myAnimator.SetTrigger("Open");
+    }
+
+    public void CloseDescription()
+    {
+        myAnimator.SetTrigger("Close");
+    }
+}
