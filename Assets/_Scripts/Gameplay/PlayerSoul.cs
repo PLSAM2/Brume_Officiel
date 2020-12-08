12de4dc4d571f403e81380d3436e6436584ffd12
ETU\v.betrancourt <valentin.betrancourt@hotmail.fr>
2020-12-08 16:32:02 +0100

    no message

--- Assets/_Scripts/Gameplay/PlayerSoul.cs
+++ Assets/_Scripts/Gameplay/PlayerSoul.cs
@@ -1,8 +1,10 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using static GameData;
 
+[Serializable]
 public class PlayerSoul
 {
     public PlayerSoul()
