b2fb827d1e63fd126bb2ff18436fe4963c37c595
ETU\v.betrancourt <valentin.betrancourt@hotmail.fr>
2020-12-11 18:14:26 +0100

    DEBUGGER

---
+++ Assets/_Scripts/DebugPlayerListObj.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DebugPlayerListObj : MonoBehaviour
+{
+    public Text mainText;
+    public InGameDebugger debugger;
+    PlayerData p;
+
+    public void Init(PlayerData p, InGameDebugger d)
+    {
+        this.p = p;
+        debugger = d;
+        mainText.text = p.ID + " / " + p.Name + " / " + p.playerCharacter;
+    }
+
+    public void TpTo()
+    {
+
+    }
+
+    public void Kill()
+    {
+
+    }
+
+    public void Heals()
+    {
+
+    }
+}
