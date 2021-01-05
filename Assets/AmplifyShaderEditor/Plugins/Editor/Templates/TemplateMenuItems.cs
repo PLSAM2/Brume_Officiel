b1040caee82f25b3310049e00ec95d16ba6b0c45
ETU\h.strobbe <h.strobbe@rubika-edu.com>
2021-01-05 15:08:48 +0100

    no message

--- Assets/AmplifyShaderEditor/Plugins/Editor/Templates/TemplateMenuItems.cs
+++ Assets/AmplifyShaderEditor/Plugins/Editor/Templates/TemplateMenuItems.cs
@@ -11,6 +11,11 @@ namespace AmplifyShaderEditor
 		{
 			AmplifyShaderEditorWindow.CreateConfirmationTemplateShader( "0770190933193b94aaa3065e307002fa" );
 		}
+		[MenuItem( "Assets/Create/Amplify Shader/HD/Lit", false, 85 )]
+		public static void ApplyTemplateHDLit()
+		{
+			AmplifyShaderEditorWindow.CreateConfirmationTemplateShader( "53b46d85872c5b24c8f4f0a1c3fe4c87" );
+		}
 		[MenuItem( "Assets/Create/Amplify Shader/Legacy/Post Process", false, 85 )]
 		public static void ApplyTemplateLegacyPostProcess()
 		{
@@ -50,6 +55,26 @@ namespace AmplifyShaderEditor
 		public static void ApplyTemplateLegacySamplesDoublePassUnlit()
 		{
 			AmplifyShaderEditorWindow.CreateConfirmationTemplateShader( "003dfa9c16768d048b74f75c088119d8" );
+		}
+		[MenuItem( "Assets/Create/Amplify Shader/HD/Decal", false, 85 )]
+		public static void ApplyTemplateHDDecal()
+		{
+			AmplifyShaderEditorWindow.CreateConfirmationTemplateShader( "d345501910c196f4a81c9eff8a0a5ad7" );
+		}
+		[MenuItem( "Assets/Create/Amplify Shader/HD/Fabric", false, 85 )]
+		public static void ApplyTemplateHDFabric()
+		{
+			AmplifyShaderEditorWindow.CreateConfirmationTemplateShader( "41e04be03f2c20941bc749271be1c937" );
+		}
+		[MenuItem( "Assets/Create/Amplify Shader/HD/Hair", false, 85 )]
+		public static void ApplyTemplateHDHair()
+		{
+			AmplifyShaderEditorWindow.CreateConfirmationTemplateShader( "e4fe21624ace6de4b9fbaabdda0c51de" );
+		}
+		[MenuItem( "Assets/Create/Amplify Shader/HD/Unlit", false, 85 )]
+		public static void ApplyTemplateHDUnlit()
+		{
+			AmplifyShaderEditorWindow.CreateConfirmationTemplateShader( "7f5cb9c3ea6481f469fdd856555439ef" );
 		}
 		[MenuItem( "Assets/Create/Amplify Shader/Legacy/Lit", false, 85 )]
 		public static void ApplyTemplateLegacyLit()
