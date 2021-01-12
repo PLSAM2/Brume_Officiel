f5756e24fd2a1f371cf8a963750801b536fbcb19
ETU\s.arachtingi <samarachtingi@gmail.com>
2021-01-12 17:31:21 +0100

    no message

--- Assets/_Scripts/Fog/FieldOfView.cs
+++ Assets/_Scripts/Fog/FieldOfView.cs
@@ -6,8 +6,6 @@ public class FieldOfView : MonoBehaviour
 {
 
 	public float viewRadius;
-	[Range(0, 360)]
-	public float viewAngle;
 
 	public LayerMask targetMask;
 	public LayerMask obstacleMask;
@@ -21,15 +19,6 @@ public class FieldOfView : MonoBehaviour
 
     public Dictionary<Transform, ushort> visibleInteractible = new Dictionary<Transform, ushort>();
     List<ushort> oldVisibleInteractible = new List<ushort>();
-
-    public float meshResolution;
-	public int edgeResolveIterations;
-	public float edgeDstThreshold;
-
-	public float maskCutawayDst = .1f;
-
-	public MeshFilter viewMeshFilter;
-	Mesh viewMesh;
 
 	[SerializeField] fowType myType;
 
@@ -51,14 +40,6 @@ public class FieldOfView : MonoBehaviour
 		SetListVisibleEnemy();
 	}
 
-    void InitMesh ()
-	{
-		viewMesh = new Mesh();
-		viewMesh.name = "View Mesh";
-
-		viewMeshFilter.mesh = viewMesh;
-	}
-
 	IEnumerator FindTargetsWithDelay ( float delay )
 	{
 		while (true)
@@ -71,7 +52,6 @@ public class FieldOfView : MonoBehaviour
 	public void GenerateFowStatic()
     {
 		isStatic = true;
-		DrawFieldOfView();
 	}
 
     [SerializeField] bool isStatic = false;
@@ -81,12 +61,6 @@ public class FieldOfView : MonoBehaviour
         SetListVisibleEnemy();
         SetListVisibleFx();
         SetListVisibleInteractible();
-
-        if (isStatic) { return; }
-
-		DrawFieldOfView();
-
-		//FogProjector.Instance.UpdateFog();
 	}
 
     void SetListVisibleInteractible()
@@ -211,114 +185,6 @@ public class FieldOfView : MonoBehaviour
             }
         }
     }
-
-	public void DrawFieldOfView ()
-	{
-		int stepCount = Mathf.RoundToInt(viewAngle * meshResolution);
-		float stepAngleSize = viewAngle / stepCount;
-		List<Vector3> viewPoints = new List<Vector3>();
-		ViewCastInfo oldViewCast = new ViewCastInfo();
-		for (int i = 0; i <= stepCount; i++)
-		{
-			float angle = transform.eulerAngles.y - viewAngle / 2 + stepAngleSize * i;
-			ViewCastInfo newViewCast = ViewCast(angle);
-
-			if (i > 0)
-			{
-				bool edgeDstThresholdExceeded = Mathf.Abs(oldViewCast.dst - newViewCast.dst) > edgeDstThreshold;
-				if (oldViewCast.hit != newViewCast.hit || (oldViewCast.hit && newViewCast.hit && edgeDstThresholdExceeded))
-				{
-					EdgeInfo edge = FindEdge(oldViewCast, newViewCast);
-					if (edge.pointA != Vector3.zero)
-					{
-						viewPoints.Add(edge.pointA);
-					}
-					if (edge.pointB != Vector3.zero)
-					{
-						viewPoints.Add(edge.pointB);
-					}
-				}
-
-			}
-
-
-			viewPoints.Add(newViewCast.point);
-			oldViewCast = newViewCast;
-		}
-
-		int vertexCount = viewPoints.Count + 1;
-		Vector3[] vertices = new Vector3[vertexCount];
-		int[] triangles = new int[(vertexCount - 2) * 3];
-
-		vertices[0] = Vector3.zero;
-		for (int i = 0; i < vertexCount - 1; i++)
-		{
-			vertices[i + 1] = transform.InverseTransformPoint(viewPoints[i]) + Vector3.forward * maskCutawayDst;
-
-			if (i < vertexCount - 2)
-			{
-				triangles[i * 3] = 0;
-				triangles[i * 3 + 1] = i + 1;
-				triangles[i * 3 + 2] = i + 2;
-			}
-		}
-
-        if(viewMesh == null)
-        {
-            InitMesh();
-        }
-
-		viewMesh.Clear();
-
-		viewMesh.vertices = vertices;
-		viewMesh.triangles = triangles;
-		viewMesh.RecalculateNormals();
-	}
-
-
-	EdgeInfo FindEdge ( ViewCastInfo minViewCast, ViewCastInfo maxViewCast )
-	{
-		float minAngle = minViewCast.angle;
-		float maxAngle = maxViewCast.angle;
-		Vector3 minPoint = Vector3.zero;
-		Vector3 maxPoint = Vector3.zero;
-
-		for (int i = 0; i < edgeResolveIterations; i++)
-		{
-			float angle = (minAngle + maxAngle) / 2;
-			ViewCastInfo newViewCast = ViewCast(angle);
-
-			bool edgeDstThresholdExceeded = Mathf.Abs(minViewCast.dst - newViewCast.dst) > edgeDstThreshold;
-			if (newViewCast.hit == minViewCast.hit && !edgeDstThresholdExceeded)
-			{
-				minAngle = angle;
-				minPoint = newViewCast.point;
-			}
-			else
-			{
-				maxAngle = angle;
-				maxPoint = newViewCast.point;
-			}
-		}
-
-		return new EdgeInfo(minPoint, maxPoint);
-	}
-
-
-	ViewCastInfo ViewCast ( float globalAngle )
-	{
-		Vector3 dir = DirFromAngle(globalAngle, true);
-		RaycastHit hit;
-
-		if (Physics.Raycast(transform.position, dir, out hit, viewRadius, obstacleMask))
-		{
-			return new ViewCastInfo(true, hit.point, hit.distance, globalAngle);
-		}
-		else
-		{
-			return new ViewCastInfo(false, transform.position + dir * viewRadius, viewRadius, globalAngle);
-		}
-	}
 
 	public Vector3 DirFromAngle ( float angleInDegrees, bool angleIsGlobal )
 	{
