e6cf86087a4abe0ccab800fe9cb4132e0fc107dc
ETU\s.arachtingi <samarachtingi@gmail.com>
2021-01-18 11:08:02 +0100

    new fog

--- Assets/Fog of War_URP/FogPass.cs
+++ Assets/Fog of War_URP/FogPass.cs
@@ -73,7 +73,7 @@ namespace UnityEngine.Rendering.Universal
             }
 
             mCam = Camera.main;
-            mCam.depthTextureMode = DepthTextureMode.Depth;
+            //mCam.depthTextureMode = DepthTextureMode.Depth;
         }
 
         /// <inheritdoc/>
