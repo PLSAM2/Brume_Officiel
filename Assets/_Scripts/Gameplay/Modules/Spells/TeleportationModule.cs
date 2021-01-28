47cdd86c357a498f10eaf6197dbf2a6d87549ae3
ETU\v.betrancourt <valentin.betrancourt@hotmail.fr>
2021-01-28 16:01:55 +0100

    Avancement tp re

--- Assets/_Scripts/Gameplay/Modules/Spells/TeleportationModule.cs
+++ Assets/_Scripts/Gameplay/Modules/Spells/TeleportationModule.cs
@@ -4,22 +4,63 @@ using UnityEngine;
 
 public class TeleportationModule : SpellModule
 {
+    public List<GameObject> onTpDisabled = new List<GameObject>();
+    public Transform wxTfs;
+
     public bool isTping = false;
+
+
     private void Update()
     {
         if (isTping)
         {
             if (Input.GetMouseButtonDown(0))
             {
-
+                Tp();
             }
         }
     }
+
 
     protected override void Resolution()
     {
         base.Resolution();
 
-        isTping = true;
+        ushort? wxId = GameFactory.GetPlayerCharacterInTeam(NetworkManager.Instance.GetLocalPlayer().playerTeam, GameData.Character.WuXin);
+
+        if (wxId != null)
+        {
+            wxTfs = GameManager.Instance.networkPlayers[(ushort)wxId].transform;
+
+            SetTpState(false);
+        }
+
+
+    }
+
+
+    public void Tp()
+    {
+        SetTpState(true);
+    }
+
+    // false = ON TP
+    public void SetTpState(bool v)
+    {
+        isTping = !v;
+
+        if (v == false)
+        {
+            CameraManager.Instance.SetParent(wxTfs);
+        } else
+        {
+            CameraManager.Instance.SetParent(this.transform);
+        }
+
+
+        foreach (GameObject obj in onTpDisabled)
+        {
+            obj.SetActive(v);
+        }
     }
 }
