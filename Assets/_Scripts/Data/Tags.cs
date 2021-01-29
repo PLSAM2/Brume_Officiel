e68e97def12f55a21b038aa6eaa7e21c248c706d
ETU\s.arachtingi <samarachtingi@gmail.com>
2021-01-29 14:56:05 +0100

    no message

--- Assets/_Scripts/Data/Tags.cs
+++ Assets/_Scripts/Data/Tags.cs
@@ -42,6 +42,7 @@
     public static readonly ushort KillCharacter = 560;
     public static readonly ushort AddUltimatePoint = 600;
     public static readonly ushort AddUltimatePointToAllTeam = 601;
+    public static readonly ushort WXSonarState = 602;
 
     //Anim 750 --> 769 >>
     public static readonly ushort SyncTrigger = 750;
