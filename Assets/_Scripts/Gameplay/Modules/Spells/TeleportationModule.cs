821255e8ec14b3e82d69c909031283d26d33bd53
ETU\v.betrancourt <valentin.betrancourt@hotmail.fr>
2021-01-29 10:42:41 +0100

    Scene test HUGO

--- Assets/_Scripts/Gameplay/Modules/Spells/TeleportationModule.cs
+++ Assets/_Scripts/Gameplay/Modules/Spells/TeleportationModule.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,21 +7,41 @@ public class TeleportationModule : SpellModule
 {
     public List<GameObject> onTpDisabled = new List<GameObject>();
     public Transform wxTfs;
+    public PlayerModule playerModule;
+    public float tpMaxTIme = 5;
 
-    public bool isTping = false;
+    private bool isTping = false;
+    private float timer = 0;
 
+    private Vector3 newPos = Vector3.zero;
 
     private void Update()
     {
         if (isTping)
         {
+            timer -= Time.deltaTime;
+            UiManager.Instance.tpFillImage.fillAmount = timer / tpMaxTIme;
+            if (timer <= 0)
+            {
+                Tp(true);
+            }
+
+
             if (Input.GetMouseButtonDown(0))
             {
-                Tp();
+                if (CheckCanTp())
+                {
+                    Tp(false);
+                }
+
             }
         }
     }
 
+    private bool CheckCanTp()
+    {
+        return false;
+    }
 
     protected override void Resolution()
     {
@@ -34,13 +55,18 @@ public class TeleportationModule : SpellModule
 
             SetTpState(false);
         }
-
-
     }
 
-
-    public void Tp()
+    public void Tp(bool isTimeEnded)
     {
+        if (isTimeEnded)
+        {
+            this.transform.position = wxTfs.position;
+        } else
+        {
+            this.transform.position = newPos;
+        }
+
         SetTpState(true);
     }
 
@@ -48,15 +74,20 @@ public class TeleportationModule : SpellModule
     public void SetTpState(bool v)
     {
         isTping = !v;
+        UiManager.Instance.tpFillImage.gameObject.SetActive(!v);
 
         if (v == false)
         {
+            timer = tpMaxTIme;
             CameraManager.Instance.SetParent(wxTfs);
-        } else
-        {
-            CameraManager.Instance.SetParent(this.transform);
+            playerModule.AddState(En_CharacterState.Stunned);
         }
-
+        else
+        {
+            timer = 0;
+            CameraManager.Instance.SetParent(this.transform);
+            playerModule.RemoveState(En_CharacterState.Stunned);
+        }
 
         foreach (GameObject obj in onTpDisabled)
         {
