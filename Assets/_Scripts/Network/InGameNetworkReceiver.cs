cab3e8341c48578dbb9c4a72a9157a1b65dd3121
ETU\v.betrancourt <valentin.betrancourt@hotmail.fr>
2021-02-02 18:37:08 +0100

    Nouveau début de game

--- Assets/_Scripts/Network/InGameNetworkReceiver.cs
+++ Assets/_Scripts/Network/InGameNetworkReceiver.cs
@@ -169,7 +169,16 @@ public class InGameNetworkReceiver : MonoBehaviour
             {
                 TpInServer(sender, e);
             }
+            else if (message.Tag == Tags.DynamicWallState)
+            {
+                DynamicWallState(sender, e);
+            }
         }
+    }
+
+    private void DynamicWallState(object sender, MessageReceivedEventArgs e)
+    {
+        GameManager.Instance.dynamicWalls.SetDoorState(false);
     }
 
     private void TpInServer(object sender, MessageReceivedEventArgs e)
