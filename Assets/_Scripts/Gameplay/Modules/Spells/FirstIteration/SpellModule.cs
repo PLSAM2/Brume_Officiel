be81d64a9e093e50133ff448aa82a8a1959624a7
Pierre Soriot <pierre.sorito@gmail.com>
2021-02-16 17:04:29 +0100

    autel better hitbox, feedback on ui

--- Assets/_Scripts/Gameplay/Modules/Spells/FirstIteration/SpellModule.cs
+++ Assets/_Scripts/Gameplay/Modules/Spells/FirstIteration/SpellModule.cs
@@ -342,7 +342,10 @@ public class SpellModule : MonoBehaviour
 	protected virtual void AddCharge ()
 	{
 		if (isOwner)
+		{
+			UiManager.Instance.CooldownReady(actionLinked);
 			AudioManager.Instance.Play2DAudio(AudioManager.Instance.cooldownUpSound, 1);
+		}
 		charges++;
 	}
 	protected virtual void UpgradeSpell () { }
