30ecb8a7b779ffa727355fb8d7eeb55e44fcd8c7
Corgy <a.ryan@rubika-edu.com>
2021-02-16 16:46:04 +0100

    Merge branch 'main' of https://github.com/PLSAM2/Brume_Officiel into main
    # Conflicts:
    #	UserSettings/EditorUserSettings.asset

---
+++ Assets/_Scripts/Lobby/LoadingGameScene.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadingGameScene : MonoBehaviour
+{
+    public Image progressBar;
+    public float fakeLoadingTime = 3;
+    private string scene;
+    private void Awake()
+    {
+        scene = RoomManager.Instance.gameScene;
+    }
+
+    void Start()
+    {
+        StartCoroutine(FakeLoadingTime());
+    }
+
+    private IEnumerator FakeLoadingTime()
+    {
+        yield return new WaitForSeconds(fakeLoadingTime);
+        StartCoroutine(LoadSceneAsyncOperation());
+    }
+
+
+    private IEnumerator LoadSceneAsyncOperation()
+    {
+        AsyncOperation gameLevelLoad = SceneManager.LoadSceneAsync(scene);
+
+        while (gameLevelLoad.progress < 1)
+        {
+            progressBar.fillAmount = gameLevelLoad.progress;
+            yield return new WaitForEndOfFrame();
+        }
+
+    }
+}
---
+++ Assets/_Scripts/UI/UltiBar.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UltiBar : MonoBehaviour
+{
+    [SerializeField] GameObject prefabUlti;
+
+    public void Init(Sc_ProjectileSpell myUltiSpell)
+    {
+
+    }
+}
