841d77f32a26381038d478e7ffdd3ff74572bc8c
ETU\s.arachtingi <samarachtingi@gmail.com>
2021-02-23 10:12:10 +0100

    fix lobby

--- Assets/_Scripts/Sam/samTest.cs
+++ Assets/_Scripts/Sam/samTest.cs
@@ -2,17 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using static GameData;
 
 public class samTest : MonoBehaviour
 {
-    public Transform posEnd;
+    Vector3 newNetorkPos;
+    public Vector3 oldPos;
 
-    public float speed = 2.5f;
+    [SerializeField] Animator animator;
 
+    public float speedoo = 00.01f;
 
     private void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, posEnd.position, speed);
+        //newNetorkPos += new Vector3( Input.GetAxis("Horizontal") * speed, 0, Input.GetAxis("Vertical") * speed);
+
+        //transform.position = Vector3.MoveTowards(transform.position, newNetorkPos, Vector3.Distance(transform.position, newNetorkPos) * 10);
+        DoAnimation();
+    }
+
+    private void DoAnimation()
+    {
+        float velocityX = (transform.position.x - oldPos.x) / Time.deltaTime;
+        float velocityZ = (transform.position.z - oldPos.z) / Time.deltaTime;
+
+        float speed = 4;
+
+        velocityX = velocityX / speed;
+        velocityZ = velocityZ / speed;
+
+        Vector3 pos = Vector3.Lerp(oldPos, new Vector3(velocityX, 0, velocityZ), Time.deltaTime * speedoo);
+
+        float right = Vector3.Dot(transform.right, pos);
+        float forward = Vector3.Dot(transform.forward, pos);
+
+        animator.SetFloat("Forward", forward);
+        animator.SetFloat("Turn", right);
+
+        oldPos = transform.position;
     }
 }
