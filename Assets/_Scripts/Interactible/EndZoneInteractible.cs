f78e3ddf82adc505933b43876977d0f447fe4c2d
ETU\v.betrancourt <valentin.betrancourt@hotmail.fr>
2021-03-01 16:38:57 +0100

    Fix bug condition de victoire / fix bug interactible

--- Assets/_Scripts/Interactible/EndZoneInteractible.cs
+++ Assets/_Scripts/Interactible/EndZoneInteractible.cs
@@ -5,5 +5,15 @@ using UnityEngine;
 
 public class EndZoneInteractible : Interactible
 {
-       // ADD BEHAVIOUR 
+    [SerializeField] GameObject waypointEndZonePrefab;
+    Waypoint waypointObj;
+
+    public override void Unlock()
+    {
+        waypointObj = Instantiate(waypointEndZonePrefab, UiManager.Instance.parentWaypoint).GetComponent<Waypoint>();
+        waypointObj.target = transform;
+        waypointObj.SetImageColor(Color.red);
+
+        base.Unlock();
+    }
 }
