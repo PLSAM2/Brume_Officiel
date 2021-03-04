87830d67cfc073187e30da8ca1b291ef72af04db
Pierre Soriot <pierre.sorito@gmail.com>
2021-03-04 11:41:06 +0100

    feedback extensions to play sounds and shake screen. wip camera

--- Assets/_Scripts/Gameplay/Extensions/SpellFeedback.cs
+++ Assets/_Scripts/Gameplay/Extensions/SpellFeedback.cs
@@ -6,11 +6,32 @@ public class SpellFeedback : MonoBehaviour
 {
 
 	public PlayerModule myPlayerModule;
+	float shakingTime = .15f, heardDistance = 7;
 
-    public void PlaySound(AudioClip _audioToPlay)
+	public void PlaySound(AudioClip _audioToPlay)
 	{
 		AudioManager.Instance.Play3DAudio(_audioToPlay, transform.position, myPlayerModule.mylocalPlayer.myPlayerId, true);
 	}
 
+	public void SetShakingTime(float _setShakingTime)
+	{
+		shakingTime = _setShakingTime;
+	}
+	public void SetHeardDistance ( float _setHeardDistance )
+	{
+		heardDistance = _setHeardDistance;
+	}
+	public void ShakeScreen(float _shakingStrength )
+	{
+		Transform player = GameFactory.GetActualPlayerFollow().transform;
+
+		if (player != null && Vector3.Distance(player.position, transform.position) < heardDistance)
+		{
+			CameraManager.Instance.SetNewCameraShake(shakingTime, _shakingStrength);
+		}
+
+	}
 
 }
+
+
