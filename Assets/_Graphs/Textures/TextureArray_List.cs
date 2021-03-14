9effc63bb179dce00da7f83c8bf12c8b4f51f025
Hugo <strobbehugo@gmail.com>
2021-03-14 16:56:22 +0100

    wip graph

---
+++ Assets/_Graphs/Textures/TextureArray_List.cs
@@ -0,0 +1,31 @@
+            Texture Array List
+
+    EnvironmentObjectShader
+
+TextureArray_Textures : 
+
+    1. Albedo et Opacity dans le channel Alpha
+    2. Normal
+    3. Specular Albedo et Mask dans le channel Alpha
+    4. RimLight Albedo et Mask dans le channel Alpha
+
+UV2 _TextureArray_Textures :
+
+    1. Albedo 
+    2. Normal
+    3. Highlight
+
+TextureArray_Grunges : 
+
+    1. AnimatedGrunge
+    2. PaintGrunge
+    3. InBrume ShadowGrunge
+    4. InBrume NormalGrunge
+
+TextureArray_Noises : 
+
+    1. Noise
+    2. WindNoise
+    3. InBrume DrippingNoise
+    4. TerrainBlendingNoise
+    5. TopTextureNoise
