295c4185fe686fd9287beba96e95e54b8212a763
ETU\v.betrancourt <valentin.betrancourt@hotmail.fr>
2021-03-22 10:46:08 +0100

    Fix training

--- Assets/_Scripts/Gameplay/Modules/Essential/PlayerModule.cs
+++ Assets/_Scripts/Gameplay/Modules/Essential/PlayerModule.cs
@@ -338,7 +338,7 @@ public class PlayerModule : MonoBehaviour
 
 		if (mylocalPlayer.isOwner)
 		{
-			if (willListenInputs && !UiManager.Instance.chat.isFocus && !GameManager.Instance.menuOpen)
+
 			{
 				//direction des fleches du clavier 
 				DirectionInputedUpdate?.Invoke(directionInputed());
