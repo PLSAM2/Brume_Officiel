a2721be1286e94a6872a54d1013a67f0dfe12476
ETU\s.arachtingi <samarachtingi@gmail.com>
2021-03-22 15:10:46 +0100

    no message

--- Assets/_Scripts/Gameplay/Modules/Essential/PlayerModule.cs
+++ Assets/_Scripts/Gameplay/Modules/Essential/PlayerModule.cs
@@ -320,7 +320,7 @@ public class PlayerModule : MonoBehaviour
 		else
 			mylocalPlayer.myUiPlayerManager.HidePseudo(false);
 
-		if (mylocalPlayer.isOwner)
+		if (mylocalPlayer.isOwner && !UiManager.Instance.chat.isFocus && !GameManager.Instance.menuOpen)
 		{
 
 			{
