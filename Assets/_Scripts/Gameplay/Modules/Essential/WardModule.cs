079b6de712153bcc56c34f928f8db5c9719bdf44
Pierre Soriot <pierre.sorito@gmail.com>
2021-05-19 16:49:53 +0200

    WARD plus pratique pour poser

--- Assets/_Scripts/Gameplay/Modules/Essential/WardModule.cs
+++ Assets/_Scripts/Gameplay/Modules/Essential/WardModule.cs
@@ -59,7 +59,7 @@ public class WardModule : SpellModule
 		}
 	}
 
-	protected override void Resolution (  )
+	protected override void Resolution ()
 	{
 		if (isLaunched)
 		{
@@ -71,7 +71,7 @@ public class WardModule : SpellModule
 		float _distance = Mathf.Clamp(Vector3.Distance(mousePosInputed, transform.position), 0, spell.range);
 
 		destination = transform.position + myPlayerModule.directionOfTheMouse() * _distance;
-			//myPlayerModule.ClosestFreePos(Vector3.Normalize(mousePosInputed - transform.position), _distance);
+		//myPlayerModule.ClosestFreePos(Vector3.Normalize(mousePosInputed - transform.position), _distance);
 
 		using (DarkRiftWriter _writer = DarkRiftWriter.Create())
 		{
@@ -87,7 +87,7 @@ public class WardModule : SpellModule
 			}
 		}
 
-		InitWardLaunch(destination);
+		InitWardLaunch(TryToFindFreePos(destination, 0));
 	}
 
 	public void InitWardLaunch ( Vector3 destination )
@@ -146,6 +146,48 @@ public class WardModule : SpellModule
 			transform.position + myPlayerModule.directionOfTheMouse() * Mathf.Clamp(Vector3.Distance(transform.position, myPlayerModule.mousePos()), 0, spell.range));
 
 		myRangePreview.Init(spell.range, CirclePreview.circleCenter.center, transform.position);
+	}
+
+	Vector3 TryToFindFreePos ( Vector3 _locationToFindFrom, int _iteration )
+	{
+		Collider[] _hits;
+		Vector3 posToCheck = _locationToFindFrom + (transform.position - _locationToFindFrom).normalized * _iteration * .6f;
+
+		_hits = Physics.OverlapCapsule(posToCheck + Vector3.down * 10,
+			posToCheck + Vector3.up * 10,
+			.6f,
+			1 << 9);
+
+		Debug.DrawLine(posToCheck + Vector3.down * 10,
+			posToCheck + Vector3.up * 10,
+			Color.red,
+			100);
+
+		if (_hits.Length == 0)
+		{
+			return posToCheck;
+		}
+		else
+		{
+
+			posToCheck = _locationToFindFrom - (transform.position - _locationToFindFrom).normalized * _iteration * .6f;
+			_hits = Physics.OverlapCapsule(posToCheck + Vector3.down * 10,
+			posToCheck + Vector3.up * 10,
+			.6f,
+			1 << 9);
+
+			Debug.DrawLine(posToCheck + Vector3.down * 10,
+						posToCheck + Vector3.up * 10,
+						Color.red,
+						100);
+
+			if (_hits.Length == 0)
+			{
+				return posToCheck;
+			}
+			else
+				return TryToFindFreePos(_locationToFindFrom, _iteration + 1);
+		}
 	}
 
 }
