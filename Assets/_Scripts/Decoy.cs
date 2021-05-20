ef60029892a1c0b4cb10df3cd190e14393379ca2
ETU\s.arachtingi <samarachtingi@gmail.com>
2021-05-20 13:52:57 +0200

    no message

--- Assets/_Scripts/Decoy.cs
+++ Assets/_Scripts/Decoy.cs
@@ -66,7 +66,10 @@ public class Decoy : MonoBehaviour, Damageable
 		myFootStep.isDecoy = true;
 		myFootStep.myDecoy = this;
 
-		myUI.Init(myTeam, _tempData.Name, GameManager.Instance.networkPlayers[_tempData.ID].liveHealth, reParameter.maxHealth);
+        int hpMax = reParameter.maxHealth;
+        hpMax += GameFactory.GetBonusHp(_tempData.ID);
+
+        myUI.Init(myTeam, _tempData.Name, GameManager.Instance.networkPlayers[_tempData.ID].liveHealth, hpMax);
 
 		if (NetworkManager.Instance.GetLocalPlayer().playerTeam != myTeam)
 		{
