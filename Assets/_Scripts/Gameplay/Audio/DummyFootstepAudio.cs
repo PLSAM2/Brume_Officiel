dd90fef6ba51b90f541b688ab86c09a50c3f8474
ETU\s.arachtingi <samarachtingi@gmail.com>
2021-06-02 10:33:49 +0200

    no message

--- Assets/_Scripts/Gameplay/Audio/DummyFootstepAudio.cs
+++ Assets/_Scripts/Gameplay/Audio/DummyFootstepAudio.cs
@@ -52,7 +52,12 @@ public class DummyFootstepAudio : MonoBehaviour
         {
             doSound = false;
             StartCoroutine(WaitEndSound(allFootsteps[Random.Range(0, allFootsteps.Length)]));
-            LocalPoolManager.Instance.SpawnNewGenericInLocal(6, transform.position, Random.Range(0, 90), 1, 0.7f);
+            //LocalPoolManager.Instance.SpawnNewGenericInLocal(6, transform.position, Random.Range(0, 90), 1, 0.7f);
+
+            if(GameFactory.GetActualPlayerFollow() != null && GameFactory.GetActualPlayerFollow().myPlayerModule.isInBrume)
+            {
+
+            }
         }
 
         IEnumerator WaitEndSound ( AudioClip _clip )
