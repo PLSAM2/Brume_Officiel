c13acadfc754b649d6e3b8d636a8f4f003ec5f17
ETU\v.betrancourt <valentin.betrancourt@hotmail.fr>
2021-06-15 14:14:37 +0200

    j'ai retiré les débugs

--- Assets/_Scripts/Network/LocalPlayer.cs
+++ Assets/_Scripts/Network/LocalPlayer.cs
@@ -200,28 +200,28 @@ public class LocalPlayer : MonoBehaviour, Damageable
 
 	void Debug ()
 	{
-		if (Input.GetKeyDown(KeyCode.K) && isOwner && !UiManager.Instance.chat.isFocus && !GameManager.Instance.menuOpen)
-		{
-			DamagesInfos _temp = new DamagesInfos();
-			_temp.damageHealth = 1;
-			DealDamages(_temp, transform, null, true, true);
-		}
+		//if (Input.GetKeyDown(KeyCode.K) && isOwner && !UiManager.Instance.chat.isFocus && !GameManager.Instance.menuOpen)
+		//{
+		//	DamagesInfos _temp = new DamagesInfos();
+		//	_temp.damageHealth = 1;
+		//	DealDamages(_temp, transform, null, true, true);
+		//}
 
 
-		if (Input.GetKeyDown(KeyCode.O) && isOwner && !UiManager.Instance.chat.isFocus && !GameManager.Instance.menuOpen)
-		{
-			AddHitPoint(1);
-		}
+		//if (Input.GetKeyDown(KeyCode.O) && isOwner && !UiManager.Instance.chat.isFocus && !GameManager.Instance.menuOpen)
+		//{
+		//	AddHitPoint(1);
+		//}
 
 		if (Input.GetKeyDown(KeyCode.P) && isOwner && !UiManager.Instance.chat.isFocus && !GameManager.Instance.menuOpen)
 		{
 			transform.position = (GameManager.Instance.GetSpawnsOfTeam(GameFactory.GetOtherTeam(RoomManager.Instance.actualRoom.playerList[myPlayerId].playerTeam)))[0].transform.position;
 		}
 
-		if (Input.GetKeyDown(KeyCode.L) && isOwner)
-		{
-			addLife_blue_fx.Play();
-		}
+		//if (Input.GetKeyDown(KeyCode.L) && isOwner)
+		//{
+		//	addLife_blue_fx.Play();
+		//}
 	}
 
 	internal void AllCharacterSpawn ()
